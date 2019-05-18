c88f29cb62b9cf9c74dcd480a48edda8180fb5da
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-18 02:46:36 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,12 @@
 def my_select(collection)
  # your code here!
+ moded_collection = []
   i = 0 
   while i < collection.length 
     if yield collection[i] == true 
-      collection[i]
+      moded_collection.push(collection[i])
       i += 1
     end
   end
-  
+  moded_collection
 end
